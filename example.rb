f39c719e28af7468c785c908e10d630be9583002
Roger Campos <roger@itnig.net>
2013-08-21 12:59:17 +0200

    Working example with actors for all the shared data + waiting for all the threads to terminate

--- example.rb
+++ example.rb
@@ -2,49 +2,58 @@ require 'celluloid'
 
 class Camaloon
   def self.currency
-    @@currency
+    Thread.current[:_camaloon_currency] ||= "xxxx"
   end
 
   def self.currency=(v)
-    @@currency = v
+    Thread.current[:_camaloon_currency] = v
   end
 end
 
-Camaloon.currency = "eur"
 
-
-$sent_emails = []
-
-class Worker
+class Storage
   include Celluloid
   attr_reader :sent_emails
 
-  def email(new_currency)
+  def initialize
+    @sent_emails = []
+  end
+
+  def push(x)
+    @sent_emails << x
+  end
+end
+
+class Worker
+  include Celluloid
+
+  def initialize(curr)
+    @curr = curr
+  end
+
+  def email(st)
     prev = Camaloon.currency
-    Camaloon.currency = new_currency
+    Camaloon.currency = @curr
     # Do expensive work here
     new_cur = Camaloon.currency
-    #puts "-"*11
-    #puts "Thread #{Thread.current.object_id} is about to set currency #{new_cur}"
-    $sent_emails << new_cur
-    #puts "Thread #{Thread.current.object_id} finished setting currency #{new_cur}"
+    st.async.push(new_cur)
     Camaloon.currency = prev
   end
 end
 
 
-5.times.map do
-  %w(eur gpb us chp).map do |curr|
-    worker = Worker.new
-    1000.times do
-      worker.async.email(curr)
-    end
-  end
-end
+storage = Storage.new
+valid_currencies = %w(eur gpb us chp)
 
-p $sent_emails.select{|x| x =="eur"}.size
-p $sent_emails.select{|x| x =="gpb"}.size
-p $sent_emails.select{|x| x =="us"}.size
-p $sent_emails.select{|x| x =="chp"}.size
+5.times.map do
+  valid_currencies.map do |curr|
+    worker = Worker.pool(args: curr)
+  end
+end.flatten.map{|x| 100.times.map { x.future.email(storage) }}.flatten.each(&:value)
+
+p storage.sent_emails.select{|x| x =="eur"}.size
+p storage.sent_emails.select{|x| x =="gpb"}.size
+p storage.sent_emails.select{|x| x =="us"}.size
+p storage.sent_emails.select{|x| x =="chp"}.size
 
 # Expected output: 5000 for each.
